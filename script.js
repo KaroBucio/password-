6809baca77c458a3f61cadd3e4cfa374995e6cbd
KaroBucio <99karbuc@gmail.com>
2022-08-30 06:45:11 -0500

    password length completed

--- script.js
+++ script.js
@@ -1,5 +1,40 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+function generatePassword(){ 
+  // to be done
+//   GIVEN I need a new, secure password
+// WHEN I click the button to generate a password
+// THEN I am presented with a series of prompts for password criteria
+// WHEN prompted for password criteria
+// THEN I select which criteria to include in the password
+// WHEN prompted for the length of the password
+// THEN I choose a length of at least 8 characters and no more than 128 characters
+// WHEN asked for character types to include in the password
+// THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+// WHEN I answer each prompt
+// THEN my input should be validated and at least one character type should be selected
+// WHEN all prompts are answered
+// THEN a password is generated that matches the selected criteria
+// WHEN the password is generated
+// THEN the password is either displayed in an alert or written to the page
+  console.log("in generate password");
+
+//variables
+var passwordLength;
+
+
+//seris of prompts
+//prompt for password length 
+passwordLength = parseInt(window.prompt("passwordLength between 8 and 128"))
+console.log("passwordLength = ", passwordLength);
+if (passwordLength < 8 || passwordLength > 128 ){
+  alert("passwordLength must be between 8 and 128. Please try again.");
+  return("password not generated. Try again.");
+  
+}
+
+}
+
 
 // Write password to the #password input
 function writePassword() {
